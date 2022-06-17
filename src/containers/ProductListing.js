41c4b6d5e6739828090a37a9ed7818e3d8e79329
happy8131 <lion8131@naver.com>
2022-06-17 23:17:08 +0900

    [Add] Redux Thunk Middleware

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -19,6 +19,7 @@ const ProductListing = () => {
 
   useEffect(() => {
     dispatch(fetchProducts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   console.log("Products:", products);
